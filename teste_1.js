7c6a4e170f166d639269141375dc4ac4791a14ab
cristhiandevgo <ivancristhian@hotmail.com>
2022-06-20 14:50:19 -0300

    update teste_1

--- teste_1.js
+++ teste_1.js
@@ -1,3 +1,25 @@
+/*
+
+Dado um vetor que guarda o valor de faturamento diário de uma distribuidora de todos os dias de um ano, faça um programa, na linguagem que desejar, que calcule e retorne:
+
+O menor valor de faturamento ocorrido em um dia do ano;
+
+O maior valor de faturamento ocorrido em um dia do ano;
+
+Número de dias no ano em que o valor de faturamento diário foi superior à média anual.
+
+
+
+IMPORTANTE:
+
+Considerar o vetor já carregado com as informações de valor de faturamento.
+
+Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média.
+
+Utilize o algoritmo mais veloz que puder definir.
+
+*/
+
 // seg, ter, qua, qui, sex, sab, dom, seg
 
 let faturamento = [150, 100, 2000, 4000, 300, 0, 0, 300] // Total: 6850
